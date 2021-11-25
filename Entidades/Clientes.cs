ab2df7d69f26a84bfbfdf69352131fbcf2ae8995
JuniorBaez-ux <junior_baez@ucne.edu.do>
2021-11-25 08:36:37 -0400

    Add project files.

---
+++ Entidades/Clientes.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_FInal_Administracion_De_Sistemas.Entidades
+{
+    public class Clientes
+    {
+        [Key]
+        public int ClienteId { get; set; }
+        public string Nombres { get; set; }
+        public string Telefono { get; set; }
+        public string Direccion { get; set; }
+        public int Cedula { get; set; }
+
+        [ForeignKey("NegocioId")]
+        public virtual Negocios Negocios { get; set; }
+
+        public Clientes()
+        {
+            Cedula = 0;
+        }
+    }
+}
