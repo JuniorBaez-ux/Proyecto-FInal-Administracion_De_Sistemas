e1a457c1c890ac0a30466b3d293f0a076484a38b
JuniorBaez-ux <junior_baez@ucne.edu.do>
2021-12-02 21:59:31 -0400

    Creando la entidad TipoCondominios.

---
+++ Entidades/TipoCondominios.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_FInal_Administracion_De_Sistemas.Entidades
+{
+    public class TipoCondominios
+    {
+        [Key]
+        public int TipoCondominioId { get; set; }
+        public string Tipo { get; set; }
+    }
+}
