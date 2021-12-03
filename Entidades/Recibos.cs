300004fc0bffb767c18e087f14f401b3c39306ba
JuniorBaez-ux <junior_baez@ucne.edu.do>
2021-12-02 21:59:06 -0400

    Creando la entidad Recibos.

---
+++ Entidades/Recibos.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_FInal_Administracion_De_Sistemas.Entidades
+{
+    public class Recibos
+    {
+        [Key]
+        public int ReciboId { get; set; }
+
+        [ForeignKey("CondominioId")]
+        public int CondominioId { get; set; }
+        public virtual Condominios Condominios { get; set; }
+
+        [ForeignKey("ClienteId")]
+        public int ClienteId { get; set; }
+        public virtual Clientes Clientes { get; set; }
+    }
+}
