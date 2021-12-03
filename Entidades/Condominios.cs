7df5896b3efc93229afbb42caea57b5c920f6ec9
JuniorBaez-ux <junior_baez@ucne.edu.do>
2021-12-02 21:59:17 -0400

    Creando la entidad Condominios.

---
+++ Entidades/Condominios.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_FInal_Administracion_De_Sistemas.Entidades
+{
+    public class Condominios
+    {
+        [Key]
+        public int CondominioId { get; set; }
+        public string Nombre { get; set; }
+        public decimal Costo { get; set; }
+
+        [ForeignKey("TipoCondominiosId")]
+        public int TipoCondominiosId { get; set; }
+        public virtual TipoCondominios TipoCondominios { get; set; }
+
+        public Condominios()
+        {
+            CondominioId = 0;
+        }
+    }
+}
