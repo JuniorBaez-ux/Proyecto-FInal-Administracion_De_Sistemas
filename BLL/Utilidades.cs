2b970b3367153c56c53605b28644f32c321ba55e
JuniorBaez-ux <junior_baez@ucne.edu.do>
2021-12-02 21:58:34 -0400

    Añadiendo la clase Utilidades.

---
+++ BLL/Utilidades.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_FInal_Administracion_De_Sistemas.BLL
+{
+    public static class Utilidades
+    {
+        public static int ToInt(this object valor)
+        {
+            int retorno;
+
+            int.TryParse(valor.ToString(), out retorno);
+
+            return retorno;
+        }
+        public static Decimal ToDecimal(this string valor)
+        {
+            Decimal retorno;
+
+            Decimal.TryParse(valor, out retorno);
+
+            return retorno;
+        }
+        public static double ToDouble(this object valor)
+        {
+            double retorno;
+
+            double.TryParse(valor.ToString(), out retorno);
+
+            return retorno;
+        }
+        public static decimal Pow(object x, object y)
+        {
+            decimal retorno;
+
+            retorno = Math.Pow(x.ToDouble(), y.ToDouble()).ToString().ToDecimal();
+
+            return retorno;
+        }
+        public static decimal ToRound(this decimal x, int Decimales)
+        {
+            decimal retorno;
+
+            retorno = Math.Round(x, Decimales);
+
+            return retorno;
+        }
+    }
+}
